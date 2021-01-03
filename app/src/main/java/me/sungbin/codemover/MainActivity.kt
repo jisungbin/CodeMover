1a3e341f3985c0bc916bb5bb4e8a77def708ab07
sungbin5304 <sungbin.dev@gmail.com>
2021-01-03 19:46:51 +0900

    :dart:

--- app/src/main/java/me/sungbin/codemover/MainActivity.kt
+++ app/src/main/java/me/sungbin/codemover/MainActivity.kt
@@ -1,18 +1,43 @@
 package me.sungbin.codemover
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.sungbin.androidutils.util.Logger
+import com.sungbin.androidutils.util.PermissionUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        PermissionUtil.request(this,
+            "권한이 필요하다구!",
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE))
+
         val database = Firebase.database
-        val ref = database.getReference("code")
-        ref.setValue("AAAAA")
+        database.reference.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val value = snapshot.getValue(String::class.java)
+                Logger.w("onChildAdded", value)
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val value = snapshot.getValue(String::class.java)
+                Logger.w("onChildChanged", value)
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {}
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
 
     }
 }
